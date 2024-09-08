6a78244283eb41b4ab3cb46be52fadd13947b647
sukrukemuk <sukru@kadriyemacit.com>
2024-09-08 21:57:12 +0300

    version 1.1.0

--- mysql.js
+++ mysql.js
@@ -1,9 +1,10 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
 });
 
